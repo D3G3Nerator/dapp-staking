a648b9d4f285b1efbec4d225024ddf40ff54bbb8
Philippe <philippe@five.team>
2021-06-14 21:26:00 +0200

    Updates

---
+++ precache-manifest.4be08148433f829eef4d56a73dc79d45.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91c326b0184b01a9dc26537b1bff42e8",
+    "url": "/dapp-staking/index.html"
+  },
+  {
+    "revision": "f9ca8e23a0232f12a53a",
+    "url": "/dapp-staking/static/css/2.37c27f5c.chunk.css"
+  },
+  {
+    "revision": "ddc2d20154cabd2ba551",
+    "url": "/dapp-staking/static/css/main.84b2b9a5.chunk.css"
+  },
+  {
+    "revision": "f9ca8e23a0232f12a53a",
+    "url": "/dapp-staking/static/js/2.2d1ba557.chunk.js"
+  },
+  {
+    "revision": "ddc2d20154cabd2ba551",
+    "url": "/dapp-staking/static/js/main.8c90204b.chunk.js"
+  },
+  {
+    "revision": "f90f1254d370b7ffc86a",
+    "url": "/dapp-staking/static/js/runtime-main.70f4e8b1.js"
+  }
+]);
